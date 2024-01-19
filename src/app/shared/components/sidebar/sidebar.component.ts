76c6a5510eb6627925a1f31ebb235c4220c252f2
AbAr <abar.f.dev@gmail.com>
2024-01-18 19:22:32 -0600

    gifs app service

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+  constructor(private gifsService: GifsService){}
+
+  // private history:string[] = [...this.gifsService.tagsHistory]
+  get tags(){
+    return this.gifsService.tagsHistory
+  }
+ }
